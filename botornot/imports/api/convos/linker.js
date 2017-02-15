ddfbe335b3f8d9f32dd5ffb3680a771505487ea3
philkuz <superpker@gmail.com>
2017-02-15 10:30:30 -0800

    Add ratings to the project.

--- botornot/imports/api/convos/linker.js
+++ botornot/imports/api/convos/linker.js
@@ -1,13 +1,12 @@
 import { Convos } from './convos.js';
 import { Meteor } from 'meteor/meteor';
 import { Messages } from '../messages/messages.js';
-
+import { Ratings } from '../ratings/ratings.js';
 Convos.addLinks({
     messages: {
         type: 'many',
         collection: Messages,
         field: 'messageId', // optional, it generates a unique one
-     //   metadata: {}, // optional, use it for meta relationships
         index: true, // optional, if set to true will create indexes for the links
         autoremove: false, // if set to true it will remove the linked objects from database
     },
@@ -15,8 +14,13 @@ Convos.addLinks({
         type: 'many',
         collection: Meteor.users,
         field: 'userId', // optional, it generates a unique one
-        // metadata: {}, // optional, use it for meta relationships
         index: true, // optional, if set to true will create indexes for the links
         autoremove: false, // if set to true it will remove the linked objects from database
+    },
+    ratings : {
+        type: 'many',
+        collection: Ratings,
+        field: 'ratingId',
+        autoremove: false,
     }
 });
