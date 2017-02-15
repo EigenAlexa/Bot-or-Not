19d67c33e54dbdc426c900b5a0467d5e50e90e00
philkuz <superpker@gmail.com>
2017-02-14 17:31:27 -0800

    Setup the Users collection on database

--- botornot/imports/startup/server/accounts.js
+++ botornot/imports/startup/server/accounts.js
@@ -4,5 +4,12 @@ Accounts.onCreateUser(function (options, user) {
     //    // to pass your schema validation
     console.log(user);
     console.log(options);
+    // user.profile = {
+    //     sessions: 0,
+	//     notratings: 0,
+    //     conversations: [],
+    //     online: false,
+    //     in_convo: false
+    // };
     return user;
 })
