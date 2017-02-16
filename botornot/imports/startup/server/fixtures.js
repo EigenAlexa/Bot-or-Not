ca8a39de235b4612c05b7a714fd7585f9d163295
Neel Kant <kantneel@berkeley.edu>
2017-02-16 15:53:59 -0800

    fixtures

---
+++ botornot/imports/startup/server/fixtures.js
@@ -0,0 +1,12 @@
+import { seedCollections } from "meteor/maxjohansen:collection-faker";
+import { Convos } from '/imports/api/convos/convos.js';
+import { Messages } from '/imports/api/messages/messages.js';
+
+const collectionsToSeed = [Convos, Messages];
+
+seedCollections(collectionsToSeed, {
+  numItemsPerCollection : 15,
+});
+
+console.log(Convos.find().fetch());
+console.log(Messages.find().fetch());
