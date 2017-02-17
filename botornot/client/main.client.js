ee70b0e726abfae0cbda8a4f39220e18e9211351
Neel Kant <kantneel@berkeley.edu>
2017-02-16 16:15:24 -0800

    client main

--- botornot/client/main.client.js
+++ botornot/client/main.client.js
@@ -1,8 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { seedCollections } from "meteor/maxjohansen:collection-faker";
-import { Convos } from '/imports/api/convos/convos.js';
-import { Messages } from '/imports/api/messages/messages.js'
 
 import '/imports/startup/client/routes.js';
 
@@ -15,12 +12,4 @@ Meteor.startup(function() {
 // const conversations = Meteor.subscribe("conversations");
 // const availableUsers = Meteor.subscribe("availableUsers");
 
-const collectionsToSeed = [Convos, Messages];
-
-seedCollections(collectionsToSeed, {
-  numItemsPerCollection : 15,
-});
-
-console.log(Convos.find().fetch());
-console.log(Messages.find().fetch());
 
