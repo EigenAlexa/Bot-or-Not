f99077a265badb32aaabc7ee5c96943848d1b759
philkuz <superpker@gmail.com>
2017-02-16 16:13:44 -0800

    Adding grapher v2.6

---
+++ botornot/packages/grapher/lib/links/config.schema.js
@@ -0,0 +1,45 @@
+export default new SimpleSchema({
+    type: {
+        type: String,
+        optional: true,
+        defaultValue: 'one'
+    },
+    collection: {
+        type: null,
+        blackbox: true,
+        optional: true
+    },
+    field: {
+        type: String,
+        optional: true
+    },
+    metadata: {
+        type: null,
+        blackbox: true,
+        optional: true
+    },
+    inversedBy: {
+        type: String,
+        optional: true
+    },
+    resolve: {
+        type: Function,
+        optional: true
+    },
+    index: {
+        type: Boolean,
+        defaultValue: false,
+        optional: true
+    },
+    autoremove: {
+        type: Boolean,
+        defaultValue: false,
+        optional: true
+    },
+    unique: {
+        type: Boolean,
+        defaultValue: false,
+        optional: true
+    }
+});
+
---
+++ botornot/packages/grapher/lib/query/testing/bootstrap/index.js
@@ -0,0 +1,15 @@
+import './comments/links';
+import './posts/links';
+import './authors/links';
+import './tags/links';
+import './groups/links';
+
+import Posts from './posts/collection';
+import Groups from './groups/collection';
+import Authors from './authors/collection';
+
+if (Meteor.isServer) {
+    Posts.expose();
+    Groups.expose();
+    Authors.expose();
+}
