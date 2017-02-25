98172463228604a14678d1490b9c4ee65529c154
philkuz <superpker@gmail.com>
2017-02-24 19:49:24 -0800

    Added room loading. Need to do extra filtering but gotta run for now

--- botornot/imports/api/convos/server/publications.js
+++ botornot/imports/api/convos/server/publications.js
@@ -1,4 +1,5 @@
-import { Convos } from '../convos.js'
+import { Convos } from '../convos.js';
+import { Meteor } from 'meteor/meteor';
 
 Meteor.publish('chat', (roomId) => {
     return Convos.find({
@@ -6,6 +7,42 @@ Meteor.publish('chat', (roomId) => {
     });
 });
 
+function getUserId(userName) {
+    let user = Meteor.users.findOne({'username' : userName});
+    return !!user ? user._id: user;
+}
+function getConvos(userId, bot) {
+    cursor = Convos.find({'users.id': userId, 'users.ratedBot' : bot}, {
+        sort : {time : -1},
+        limit: 5,
+        fields : {
+            _id : 1,
+            time : 1,
+            'users.ratedBot' : 1,
+        },
+        users : {$elemMatch : {'id' : userId}}
+    });
+    console.log(cursor.fetch());
+    return cursor;
+}
+
+// published all convos where user is rated bot
+Meteor.publish('userNotConvos', (userName) => {
+    // get user id from username
+    let userId = getUserId(userName);
+    console.log('userid for ', userName, userId);
+    // return all conversations relevant to that userid
+    return getConvos(userId, false);
+});
+
+Meteor.publish('userBotConvos', (userName) => {
+    // get user id from username
+    let userId = getUserId(userName);
+    // return all conversations relevant to that userid
+    return getConvos(userId, true);
+});
+
+// returns details about the open rooms
 Meteor.publish('openrooms', () => {
     return Convos.find({
         curSessions : {$lt : 2},
@@ -15,6 +52,7 @@ Meteor.publish('openrooms', () => {
         closed : 1,
     }});
 });
+
 Convos.find({closed: false}).observe({
       changed: (newConvo, oldConvo) => {
         console.log('convo changed', newConvo.turns);
