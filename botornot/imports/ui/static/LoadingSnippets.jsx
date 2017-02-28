72516e5bbd5d40a0d26be92d72701764a696cd83
philkuz <superpker@gmail.com>
2017-02-27 17:02:16 -0800

    Add prompts and butt challenge

--- botornot/imports/ui/static/LoadingSnippets.jsx
+++ botornot/imports/ui/static/LoadingSnippets.jsx
@@ -2,8 +2,11 @@
 export default snippets = [
     
     "The bots are better than you think.",
+    "The bots are better than you'd expect.",
+    "Keep your conversation civil. You get more points that way.",
+    "Get your game face on.",
     "Don't be fooled if they tell you to look them up on facebook.",
-    "Don't include a lot of personal information, otherwise the game won't be fun."
+    "Don't include personal information, otherwise the game won't be fun."
 
   ];
 
---
+++ botornot/imports/ui/static/prompt-list.jsx
@@ -0,0 +1,13 @@
+export default PromptList = [
+  "What makes fake news so appealing?", 
+  "How would you have fixed the Oscar mistake?",
+  "What movie are you most excited about this year?",
+  "Tell us about any experience you've had with VR.",
+  "What do you think VR holds for the future?",
+  "Talk about your favorite musician",
+  "Talk about himalayan salt lamps.",
+  "How do you think Donald Trump would handle a scandal like Watergate?",
+  "How would life be different if Hillary Clinton won the US presidential election?",
+  "How would life be different if Bernie Sanders won the US presidential election?",
+  "What's the breakout fashion statement of 2017?",
+];
