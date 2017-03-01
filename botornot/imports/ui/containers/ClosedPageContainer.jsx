3e7c5c56c0c329601799ace4243ae4e4792a095d
JamesMBartlett <james@ml.berkeley.edu>
2017-02-28 18:55:28 -0800

    Added notification system to display ratings later

--- botornot/imports/ui/containers/ClosedPageContainer.jsx
+++ botornot/imports/ui/containers/ClosedPageContainer.jsx
@@ -2,12 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Convos } from '/imports/api/convos/convos.js';
 import ClosedPage from '../pages/ClosedPage.jsx';
+import { Session } from 'meteor/session';
 
 export default ClosedContainer = createContainer(({ params }) => {
   const { roomId, userLeft } = params;  
   const roomHandle = Meteor.subscribe('chat', roomId);
   const usersHandle = Meteor.subscribe('currentUsers', roomId);
   console.log(roomId, userLeft);
+  /*Meteor.users.find({_id: Meteor.userId()}).observe({
+    changed: (newUser, oldUser) => {
+      if(newUser.rated && !oldUser.rated){
+        Session.set('playNotification', true);
+        Session.set('rating', newUser.lastRating);
+      }
+    } 
+  });*/
   return {
     room: Convos.findOne({_id: roomId}),
     loading: !roomHandle.ready() || !usersHandle.ready(),
--- botornot/imports/ui/layouts/screen.jsx
+++ botornot/imports/ui/layouts/screen.jsx
@@ -1,7 +1,31 @@
 import React, { PropTypes } from 'react';
 import NavBar from '/imports/ui/components/NavBar.jsx';
 import Footer from '/imports/ui/components/Footer.jsx';
+import NotificationSystem from 'react-notification-system';
+
 export default class Screen extends React.Component {
+    renderRatingNotification(rating){
+      notification = {
+        title: rating == 'not' ? 'NOT' : 'BOT',
+        message: "You were just rated as a " + rating + " for a previous conversation.",
+        position: 'bc',
+        level: rating == 'not'? 'success' : 'error',
+        autoDismiss: 0,
+
+      };
+      this.refs.notificationsystem.addNotification(notification);
+      Session.set('playNotification', "false");
+    }
+    componentDidMount(){ 
+      Tracker.autorun(() => {
+        playNotification = Session.get('playNotification');
+        rating = Session.get('rating');
+        if(playNotification == "true"){
+          this.renderRatingNotification(rating);
+        }
+      });
+    }
+
     render() {
         return (
             <div>
@@ -10,6 +34,7 @@ export default class Screen extends React.Component {
                 </header>
                     {this.props.children}
               <Footer />
+              <NotificationSystem ref='notificationsystem' />
             </div>
         );
     }
