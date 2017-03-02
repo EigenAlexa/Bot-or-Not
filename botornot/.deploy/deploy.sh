4b0adba390a427636ce02391640e055a45e21882
JamesMBartlett <james@ml.berkeley.edu>
2017-03-01 21:59:12 -0800

    added setting for host ID

--- botornot/.deploy/deploy.sh
+++ botornot/.deploy/deploy.sh
@@ -1,8 +1,9 @@
+export count=0;
 cat hosts | while read a;
-
 do
 echo $a;
-sed "s/HOST_IP/$a/g" mup.main.js > mup-$a.js
-mup --config mup-$a.js $1
-
+sed "s/HOST_IP/$a/g" mup.main.js > mup-$a.js;
+sed "s/HOST_ID/$count/g" settings.main.json > settings-$a.json;
+mup --config mup-$a.js --settings settings-$a.json $1 ;
+count=$((count + 1));
 done
