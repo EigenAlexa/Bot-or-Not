eec76d5098a1a1e6b34c8dc5c51710e3c740a669
philkuz <superpker@gmail.com>
2017-03-02 13:02:52 -0800

    Add notification and undo button for when marked offtopic

--- botornot/imports/ui/layouts/screen.jsx
+++ botornot/imports/ui/layouts/screen.jsx
@@ -4,6 +4,11 @@ import Footer from '/imports/ui/components/Footer.jsx';
 import NotificationSystem from 'react-notification-system';
 
 export default class Screen extends React.Component {
+    constructor(props) {
+      super(props);
+      this.renderOffTopicNotification = this.renderOffTopicNotification.bind(this);
+    }
+
     renderRatingNotification(rating){
       notification = {
         title: rating == 'not' ? 'NOT' : 'BOT',
@@ -17,12 +22,44 @@ export default class Screen extends React.Component {
       Meteor.call('users.setRated', Meteor.userId());
       Session.set('playNotification', "false");
     }
+
+    renderOffTopicNotification(){
+      this.setState({offTopic : true});
+      notification = {
+        title: 'Marked Conversation as Off Topic',
+        message: "Thanks for letting us know that the conversation was off topic.",
+        position: 'bc',
+        level: 'success',
+        autoDismiss: 5,
+        action : {
+          label: 'Undo', 
+          callback: () => {
+            this.setState({offTopic : false});
+          }
+        },
+        onRemove: () => {
+          if (this.state.offTopic) {
+            Meteor.call('convos.markOffTopic', Session.get('curConvo'), Meteor.userId());
+          }
+        }
+
+      };
+      this.refs.notificationsystem.addNotification(notification);
+      Session.set('notifyOffTopic', "false");
+      
+    }
+
     componentDidMount(){ 
       Tracker.autorun(() => {
         playNotification = Session.get('playNotification');
         rating = Session.get('rating');
         if(playNotification == "true"){
           this.renderRatingNotification(rating);
+        }
+
+        offTopicNotification = Session.get('notifyOffTopic');
+        if (offTopicNotification == "true") {
+          this.renderOffTopicNotification();
         }
       });
     }
