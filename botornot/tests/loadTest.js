365e7919aeed14461e7f421792209b70ff94d3ad
JamesMBartlett <james@ml.berkeley.edu>
2017-03-01 16:10:44 -0800

    added meteor down load testing

---
+++ botornot/tests/loadTest.js
@@ -0,0 +1,14 @@
+
+meteorDown.init(function (Meteor) {
+  Meteor.call('convos.updateChat', "hi there", "rASEfCG92mMXww89R", "sneRbex8RRqpPPbdT", function (error, result) {
+    Meteor.kill();
+  });
+  //Meteor.subscribe('currentUsers', "rASEfCG92mMXww89R", function() {
+  //  Meteor.kill();
+  //});
+});
+
+meteorDown.run({
+  concurrency: 100,
+  url: "http://beta.botornot.ml"
+});
