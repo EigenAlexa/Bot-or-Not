790293607341f4423a1759aec62c92f625f41017
JamesMBartlett <james@ml.berkeley.edu>
2017-03-08 13:18:28 -0800

    added username and ranking style to leaderboard page

--- botornot/imports/ui/containers/LeaderboardPageContainer.jsx
+++ botornot/imports/ui/containers/LeaderboardPageContainer.jsx
@@ -4,13 +4,15 @@ import LeaderboardPage from '../pages/LeaderboardPage';
 
 export default LeaderboardContainer = createContainer(() => {
   const usersHandle = Meteor.subscribe('topNUsers', 25);
-  const loading = !usersHandle.ready();
+  const userHandle = Meteor.subscribe('currentUser', Meteor.userId());
+  const loading = !usersHandle.ready() || !userHandle.ready();
   const query = {
     sort: {rating: -1},
     limit: 25,
     fields: {username: 1, rating: 1} 
   };
   return {
-    users: loading ? [] : Meteor.users.find({}, query).fetch()
+    users: loading ? [] : Meteor.users.find({}, query).fetch(),
+    currentUser: loading ? {}: Meteor.users.findOne({_id: Meteor.userId()}),
   };
 }, LeaderboardPage);
