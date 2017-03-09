deedb2d50d9ff235b6d7c399382de33207308c6c
philkuz <superpker@gmail.com>
2017-03-09 10:27:37 -0800

    Better page - design almost done

--- botornot/imports/ui/components/ConvoItem.jsx
+++ botornot/imports/ui/components/ConvoItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export const ConvoItem = ({ convo, time }) => (
-  <div className="wrapper-message">
-    {time.toString()}
+export const ConvoItem = ({ convo, time }) => {
+  date = moment(time);
+  console.log(date);
+  return <div className="wrapper-message">
+    {date.format('MMMM Do YYYY, h:mm:ss a')}
   </div>
-);
+};
 
 ConvoItem.propTypes = {
     convo: React.PropTypes.object,
