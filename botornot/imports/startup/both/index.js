92970439f822408d04da422196cee13dcbe128ff
JamesMBartlett <james@ml.berkeley.edu>
2017-03-10 15:56:37 -0800

    working flowrouter routing in tests

--- botornot/imports/startup/both/index.js
+++ botornot/imports/startup/both/index.js
@@ -2,4 +2,3 @@ import './accounts-flow.js'
 import './accounts-config.js'
 import './debug.js'
 
-Router.configure({noRoutesTemplate: ""});
--- botornot/tests/gagarin/chat_tests.js
+++ botornot/tests/gagarin/chat_tests.js
@@ -4,18 +4,35 @@ describe('chat testing suite', () => {
   client1 = browser(server); 
   client2 = browser(server);
 
+  waitForFlowRoute = (client, path) => {
+    return client.execute(function (path) {
+      FlowRouter.go(path);
+    }, [ path ])
+    .wait(10000, 'until current path is ' + path, function (path) {
+      
+      var controller = FlowRouter.current();
+      var pathOK = (window.location.pathname + window.location.search + window.location.hash === path);
+
+      if (controller && pathOK ) {
+        return true;
+      } else {
+        FlowRouter.go(path);
+      }
+    }, [ path ]);
+  }; 
 
   it('should be able to route to /chat', () => {
-    return client1.waitForRoute('/chat')
+    return waitForFlowRoute(client1, '/chat')
       .getText(".page-header")
       .then( (text) => {
         expect(text).to.include("CHAT");
       });
   });
   it('should connect two users when they both route to chat', () => {
-    return client2.waitForRoute('/chat')
+    return waitForFlowRoute(client1, '/chat')
       .then(() => {
-        return client1.waitForRoute('/chat');
+        return waitForFlowRoute(client2,'/chat');
       })
+    
   });
 });
