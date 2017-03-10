73671f87bfc8d6de1b7fbaa73f4531dff475e7be
JamesMBartlett <james@ml.berkeley.edu>
2017-03-10 15:56:37 -0800

    working flowrouter routing in tests

--- botornot/tests/gagarin/chat_tests.js
+++ botornot/tests/gagarin/chat_tests.js
@@ -102,6 +102,22 @@ describe('chat connection suite', () => {
   client1 = browser(server); 
   client2 = browser(server);
 
+  waitForFlowRoute = (client, path) => {
+    return client.execute(function (path) {
+      FlowRouter.go(path);
+    }, [ path ])
+    .wait(10000, 'until current path is ' + path, function (path) {
+      
+      var controller = FlowRouter.current();
+      var pathOK = (window.location.pathname + window.location.search + window.location.hash === path);
+
+      if (controller && pathOK ) {
+        return true;
+      } else {
+        FlowRouter.go(path);
+      }
+    }, [ path ]);
+  }; 
 
   it('should be able to route to /chat', () => {
     return waitForFlowRoute(client1, '/chat')
