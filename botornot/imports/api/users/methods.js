5ca171649c0c2cf52d06f5446b4a6c733ce4ebe1
philkuz <superpker@gmail.com>
2017-03-10 14:40:07 -0800

    Removed half finished statement

--- botornot/imports/api/users/methods.js
+++ botornot/imports/api/users/methods.js
@@ -1,24 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Convos } from '/imports/api/convos/convos.js';
-/**Meteor.methods({
-  'users.getTopN'(N) {
-    cursor = Meteor.users.find({}, {
-      sort: {rating: -1}, 
-      limit: N}
-      );
-    return cursor.fetch();  
-  }
-});
-*/
-/***
-UserPresence.onSessionDisconnected((connection) => {
-  console.log(connection);
-  console.log(Meteor.server.sessions);
-});
-**/
+
 Meteor.methods({
   'users.updateAnonymousUsername'(userId) {
-    user_no = 
     Meteor.users.update({_id: userId}, {
       $set: {username: faker.hacker.ingverb() + faker.hacker.noun(), firstTime: true}
     });
