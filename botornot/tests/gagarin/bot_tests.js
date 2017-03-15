492c72579f34175ea24f380d5b1ad7bc2a32fe93
JamesMBartlett <james@ml.berkeley.edu>
2017-03-14 17:34:47 -0700

    increased the timeout more

--- botornot/tests/gagarin/bot_tests.js
+++ botornot/tests/gagarin/bot_tests.js
@@ -7,7 +7,7 @@ describe('bots', () => {
 
   it('should connect to a user after the timeout length', () => {
     return utils.waitForFlowRoute(client1, '/chat')  
-      .waitForDOM(".message-panel", 25000) //TODO get value from meteor settings
+      .waitForDOM(".message-panel", 35000) //TODO get value from meteor settings
       .then(() => {
         return utils.waitForFlowRoute(client1, '/');
       })
@@ -15,7 +15,7 @@ describe('bots', () => {
 
   it('should send reply to user message', () => {
     return utils.waitForFlowRoute(client1, '/chat')
-      .waitForDOM(".message-panel", 20000) 
+      .waitForDOM(".message-panel", 35000) 
       .sendKeys(".form-control", "hi" + utils.ENTER)
       .then(() => {
         return utils.waitForNthDOM(client1, ".from-them", 5000, 1);  
