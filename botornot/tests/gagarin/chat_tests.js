5446e4a54e7c6565ddfbbda32817f52dc4b721b2
philkuz <superpker@gmail.com>
2017-03-16 15:05:48 -0700

    Added fixed test

--- botornot/tests/gagarin/chat_tests.js
+++ botornot/tests/gagarin/chat_tests.js
@@ -32,9 +32,6 @@ describe('chat connection suite', () => {
     return client1.wait(2000, "until user is found", () => {
         return Meteor.users.findOne({username: "tester1"});
       })
-      .then((user) => {
-        return expect(user.firstTime).to.be.false;
-      })
       .then(() => {
         return client2.wait(2000, "until user is found", () => {
           return Meteor.users.findOne({username: "tester2"});  
