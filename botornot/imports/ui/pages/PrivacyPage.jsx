317b793f51119f235291acde5acf7d38c323036a
William Guss <carlostnb@gmail.com>
2017-03-16 15:34:40 -0700

    Re containerizing website.

--- botornot/imports/ui/pages/PrivacyPage.jsx
+++ botornot/imports/ui/pages/PrivacyPage.jsx
@@ -7,11 +7,9 @@ var PrivacyPage = React.createClass({
       <div>
         <title>Bot or Not</title>
         <section className="section-background">
-          <div className="container">
             <h2 className="page-header">
               Privacy Policy
             </h2>
-          </div> {/* /.container */}
         </section> {/* /.section-background */}
         <div className="container">
         <div className='innerText'>This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.<br/>
