6c8fccbc23c372a690f4c55f636c2ffb4b325eb4
philkuz <superpker@gmail.com>
2017-03-16 15:45:43 -0700

    Add profile panel + styling; PROTIP DON"T MERGE SHIT IS BROKEN

--- botornot/imports/ui/containers/ProfileSideContainer.jsx
+++ botornot/imports/ui/containers/ProfileSideContainer.jsx
@@ -3,51 +3,42 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Convos } from '/imports/api/convos/convos.js';
 import ProfileSide from '/imports/ui/components/ProfileSide.jsx';
 
-ProfileSideContainer = createContainer(({ params: { params } }) => {
-    const username = params.username;
-    const userHandle = Meteor.subscribe('userProfile', username);
-    const notConvoHandle = Meteor.subscribe('userNotConvos', username);
-    const botConvoHandle = Meteor.subscribe('userBotConvos', username);
-    const ratingsHandle = Meteor.subscribe('users.ratings');
-    const userCursor = Meteor.users.find({username: username});
-    // userCursor.observe({
-    //   changed: (newUser, oldUser) => {
-    //     if (!newUser){
-    //       return;
-    //     }
-    //     if (!newUser.in_convo && oldUser.in_convo){
-    //       console.log("callback for user leaving: " + newUser._id);
-    //       FlowRouter.go('closed', {}, {convoId: oldUser.curConvo, userLeft: newUser.left});
-    //     }
-    //   }
-    // });
-    const loading = !userHandle.ready();
-    const user = Meteor.users.findOne({username: username});
-    const ranking = !!user && ratingsHandle.ready() ? Meteor.users.find({rating: {$gt: user.rating}}).count() + 1: -1;
-    const convosLoading = !botConvoHandle.ready() || !notConvoHandle.ready();
-    
-    const notConvos = convosLoading ? [] : Convos.find(
-      {'users.rated': 'not'},  
-      {limit : 5}).fetch();
+ProfileSideContainer = createContainer(({ username }) => {
+  if (!!username) {
+    console.log('username', username)
+    userHandle = Meteor.subscribe('userProfile', username);
+    ratingsHandle = Meteor.subscribe('users.ratings');
+    userCursor = Meteor.users.find({username: username});
+    loading = !userHandle.ready();
+    user = Meteor.users.findOne({username: username});
+    ranking = !!user && ratingsHandle.ready() ? Meteor.users.find({rating: {$gt: user.rating}}).count() + 1: -1;
 
     console.log(Convos.find().fetch(), 'finding convos');
-    const botConvos = convosLoading ? [] : Convos.find(
-      {'users.rated': 'bot'},
-      {limit : 5}).fetch();
-
-    const currentUser = loading ? {}: Meteor.users.findOne({_id: Meteor.userId()});
-    const isSelfProfile = !!currentUser &&  !!currentUser['username'] &&currentUser.username == username;
-
-    return {
-      user, 
-      username,
-      loading,
-      ranking,
-      userExists : !!user, 
-      convosLoading,
-      botConvos,
-      notConvos,
-      isSelfProfile
-    };
+    currentUser = loading ? {}: Meteor.users.findOne({_id: Meteor.userId()});
+    isSelfProfile = !!currentUser &&  !!currentUser['username'] && currentUser.username == username;
+    signedIn = !user.anon;
+  }
+  else {
+    user = null; 
+    username = null;
+    signedIn = false;
+    user = null; 
+    username = null;
+    loading = false;
+    ranking = null;
+    signedIn = false;
+    isSelfProfile = false;
+  }
+  return {
+    user, 
+    username,
+    signedIn,
+    user, 
+    username,
+    loading,
+    ranking,
+    isSelfProfile,
+    userExists : !!user, 
+  };
 }, ProfileSide);
 export {ProfileSideContainer };
