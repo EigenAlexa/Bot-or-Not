14e22a4160fc170c127cc49c4d83bd43549c4edc
philkuz <superpker@gmail.com>
2017-03-15 18:47:54 -0700

    undo that

--- botornot/imports/ui/components/Chat.jsx
+++ botornot/imports/ui/components/Chat.jsx
@@ -103,17 +103,17 @@ export default class Chat extends React.Component {
         const showBot = this.props.room.turns > Meteor.settings.public.ratingTurns && !this.props.room.closed;
         const rateButtonClass = showBot ? "" : "btn-disabled";
         return (<div className="row">
-                  <div className="col-md-offset-1 col-sm-5 col-md-3">
-                    <h4 className="word-wrap pro-tip"><b> Pro Tip: </b>{this.snippets[this.state.index] }</h4> 
-                    <Button bsStyle='primary' size='medium' className={"rate-now " + rateButtonClass} onClick={this.handleRateButton.bind(this)} >Rate Now</Button>
-                  </div>
-                  <div className="col-sm-7">
+                  <div className="col-sm-7 col-md-offset-1">
                   <div id="modal-div"> </div>
                     <ChatPanel messages={Messages}/>
                     <div className="progress-input row">
                     {this.props.room.closed ? "": this.renderChatInput()}
                     {this.props.room.closed && user.convoClosed ? this.renderClosed() : "" }
                     </div>
+                  </div>
+                  <div className="col-sm-5 col-md-3">
+                    <h4 className="word-wrap pro-tip"><b> Pro Tip: </b>{this.snippets[this.state.index] }</h4> 
+                    <Button bsStyle='primary' size='medium' className={"rate-now " + rateButtonClass} onClick={this.handleRateButton.bind(this)} >Rate Now</Button>
                   </div>
                 </div>);
 
