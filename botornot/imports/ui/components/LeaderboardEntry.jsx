9cf18aedc9db4feba487169dce0405772e9dcd5d
JamesMBartlett <james@ml.berkeley.edu>
2017-03-20 20:12:16 -0700

    fixed leaderboard ranking numbers

--- botornot/imports/ui/components/LeaderboardEntry.jsx
+++ botornot/imports/ui/components/LeaderboardEntry.jsx
@@ -1,20 +1,30 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class LeaderboardEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 'ranking': false};
+  }
   getContent() {
     console.log(this.props.user);
     return {
       username: this.props.user.username, 
       rating: this.props.user.rating,
-      number: this.props.number
+      number: this.props.number,
+      id: this.props.user._id,
     }
   }
   render() {
     content = this.getContent();
     console.log(content);
+    if (!this.state.ranking) {
+      Meteor.call('users.getUserRanking', content.id, (error, result) => {
+        this.setState({'ranking': result});
+      });
+    }
     return (
         <tr className="row">
-          <td className="col-md-1 leader-elem">{content.number}</td>
+          <td className="col-md-1 leader-elem">{this.state.ranking ? this.state.ranking : ""}</td>
           <td className="col-md-1 leader-elem"><a href={"/profile/" + content.username}>{content.username}</a></td>
           <td className="col-md-1 leader-elem leader-rating">{content.rating.toFixed(2)}</td>
         </tr>
