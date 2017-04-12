4fae1c1be22cadf90713e28567ea09def9b78172
philkuz <superpker@gmail.com>
2017-04-11 22:07:59 -0700

    Update chat to account for longer snippets

--- botornot/imports/ui/static/LoadingSnippets.jsx
+++ botornot/imports/ui/static/LoadingSnippets.jsx
@@ -2,13 +2,13 @@
 export default snippets = [
     "The bots are better than you'd expect.",
     "Keep your conversation civil. Your XP will increase faster.",
-    "Our bots learn from previous conversations. Don't be fooled if they ask you to look them up on facebook.",
+    "Our bots learn from previous conversations.",
     "Don't include personal information, otherwise you will lose out on XP.", 
     "You'll get more XP the longer conversation is. Newlines and gibberish don't count.",
     "We use machine learning to assess the quality of your conversation. The more engaging you are, the more XP you get.",
     "You'll be matched with bots and humans at your level. The more you chat, the harder it gets to distinguish the two.",
     "How human you are rated affects the the types of bots and players you're matched with.",
-    "Some bots don't use perfect grammar, spelling, or typing.",
+    "Bots don't always use perfect grammar or have proper spelling.",
     "High level bots learn from the most engaging users making them harder to identify.",
   ];
 
