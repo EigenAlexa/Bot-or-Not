0628abe2cda929ac8b769190108fd063a01db9f8
philkuz <superpker@gmail.com>
2017-04-11 21:43:50 -0700

    Update Loading snippets

--- botornot/imports/ui/static/LoadingSnippets.jsx
+++ botornot/imports/ui/static/LoadingSnippets.jsx
@@ -1,13 +1,15 @@
 
 export default snippets = [
-    
-    "The bots are better than you think.",
     "The bots are better than you'd expect.",
-    "Keep your conversation civil. You get more points that way.",
-    "Get your game face on.",
-    "Don't be fooled if they tell you to look them up on facebook.",
-    "Don't include personal information, otherwise the game won't be fun."
-
+    "Keep your conversation civil. Your XP will increase faster.",
+    "Our bots learn from previous conversations. Don't be fooled if they ask you to look them up on facebook.",
+    "Don't include personal information, otherwise you will lose out on XP.", 
+    "You'll get more XP the longer conversation is. Newlines and gibberish don't count.",
+    "We use machine learning to assess the quality of your conversation. The more engaging you are, the more XP you get.",
+    "You'll be matched with bots and humans at your level. The more you chat, the harder it gets to distinguish the two.",
+    "How human you are rated affects the the types of bots and players you're matched with.",
+    "Some bots don't use perfect grammar, spelling, or typing.",
+    "High level bots learn from the most engaging users making them harder to identify.",
   ];
 
 
